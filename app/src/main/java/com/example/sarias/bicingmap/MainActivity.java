d0b5010b0775d9d94e411115bed0f053367016ec
Sarias <53831285s@iespoblenou.org>
2018-02-15 17:14:56 +0100

    Pinto un punto con imagen en el mapa y tengo datos de la api

--- app/src/main/java/com/example/sarias/bicingmap/MainActivity.java
+++ app/src/main/java/com/example/sarias/bicingmap/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.sarias.bicingmap;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Station> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +29,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-
-        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
-        IMapController mapController = map.getController();
-        mapController.setZoom(9);
-        mapController.setCenter(startPoint);
 
     }
 
@@ -54,5 +52,27 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        refresh();
+    }
+
+    private void refresh() {
+        MainActivity.RefreshDataTask task = new MainActivity.RefreshDataTask();
+        task.execute();
+    }
+
+    private class RefreshDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            BikeAPI api = new BikeAPI();
+            result = api.getStations();
+            Log.d("result", result.toString());
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/sarias/bicingmap/MainActivityFragment.java
+++ app/src/main/java/com/example/sarias/bicingmap/MainActivityFragment.java
@@ -1,6 +1,7 @@
 package com.example.sarias.bicingmap;
 
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
 import java.util.ArrayList;
 
 /**
@@ -16,6 +23,9 @@ import java.util.ArrayList;
  */
 public class MainActivityFragment extends Fragment {
     ArrayList<Station> result;
+    MapView map;
+    GeoPoint startPoint;
+    Marker startMarker;
 
     public MainActivityFragment() {
     }
@@ -25,9 +35,52 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
+            map = (MapView) view.findViewById(R.id.map);
+
+            initialMap();
+            setZoom();
+            setMarker();
 
         return view;
+    }
+
+    private void setMarker() {
+        startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+
+        for (int i = 0; i < result.size(); i++) {
+            Station station = result.get(i);
+
+            GeoPoint position = new GeoPoint(station.getLatitude(), station.getLongitude());
+            startMarker.setPosition(position);
+            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            map.getOverlays().add(startMarker);
+
+            map.invalidate();
+            startMarker.setIcon(getResources().getDrawable(R.mipmap.ic_bicing));
+            startMarker.setTitle("Start point");
+
+        }
+
+    }
+
+    private void setZoom() {
+        startPoint = new GeoPoint(41.3903508, 2.1330763);
+        IMapController mapController = map.getController();
+        mapController.setZoom(17);
+        mapController.setCenter(startPoint);
+
+    }
+
+    private void initialMap() {
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
     }
 
 
@@ -50,6 +103,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+        Log.d("result", "Hola");
         refresh();
     }
 
@@ -63,11 +117,11 @@ public class MainActivityFragment extends Fragment {
         protected Void doInBackground(Void... voids) {
             BikeAPI api = new BikeAPI();
             result = api.getStations();
+            Log.d("Hola", result.toString());
 
             return null;
         }
     }
-
 
 
 }
